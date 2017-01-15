2d319938f41e54b244b9737a38686fdcc46b8490
Farsan <farsan.rashid@gmail.com>
2017-01-15 14:52:05 +0600

    Implemented Sigmoid Function

--- Machine Learning/Logistic Regression and Regularization/ex2/sigmoid.m
+++ Machine Learning/Logistic Regression and Regularization/ex2/sigmoid.m
@@ -4,15 +4,14 @@ function g = sigmoid(z)
 
 % You need to return the following variables correctly 
 g = zeros(size(z));
-
+for row_index = 1:size(z,1)
+    for col_index = 1: size(z,2)
+    val = z(row_index, col_index)
+    g(row_index, col_index) = 1/(1 + exp(-val))
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-
-
-
-
-
+end
 % =============================================================
 
 end
